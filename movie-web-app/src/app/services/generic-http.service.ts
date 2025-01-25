527abcacc52a1d23cd7becb177316402d0c9cf1d
uyarmelik <uyarmelik@gmail.com>
2025-01-25 04:23:35 +0300

    "movie-web-app" project has been uploaded.

---
+++ movie-web-app/src/app/services/generic-http.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.example';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GenericHttpService {
+  baseUrl: string = 'https://api.themoviedb.org/3';
+  constructor(private httpClient: HttpClient) {}
+
+  httpGet(url: string): any {
+    return this.httpClient.get<any>(`${this.baseUrl}/${url}`, {
+      headers: {
+        'Authorization': `Bearer ${environment.token}`,
+      },
+    });
+  }
+}
---
+++ movie-web-app/src/environments/environment.example.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  token: 'your_api_token_here',
+};
